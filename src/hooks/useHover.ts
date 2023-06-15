7e77dce70f03aed15a7787e63fdce88af62b3729
TMWAG <semyon.valyaev@gmail.com>
2023-06-15 17:52:59 +0300

    новый хук для обработки события hover

---
+++ src/hooks/useHover.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export default function useHover(ref: React.MutableRefObject<HTMLDivElement | null>){
+  const [isHovering, setIsHovering] = useState<boolean>(false);
+  const on = () => setIsHovering(true);
+  const off = () => setIsHovering(false);
+  useEffect(() => {
+    if (!ref.current) return;
+    const node = ref.current;
+    node.addEventListener('mouseenter', on);
+    node.addEventListener('mousemove', on);
+    node.addEventListener('mouseleave', off);
+    return () => {
+      node.removeEventListener('mouseenter', on);
+      node.removeEventListener('mousemove', on);
+      node.removeEventListener('mouseleave', off);
+    };
+  }, [ref]);
+  return isHovering;
+}
