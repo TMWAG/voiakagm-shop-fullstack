680b0c1b3c5825b4b4291aac1bb2efb0b4f6588e
TMWAG <semyon.valyaev@gmail.com>
2023-06-18 15:02:23 +0300

    рефакторинг

--- src/app/components/MiniProfile.tsx
+++ src/app/components/MiniProfile.tsx
@@ -1,21 +1,27 @@
 'use client';
 
+import { deleteCookie } from "cookies-next";
 import { useRouter } from "next/navigation";
 
 export default function MiniProfile() {
   const router = useRouter();
-  const onLogout = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
-    fetch('/api/auth/logout', { method: 'GET' })
-    .then(() => router.refresh());
-  };
+  const logout = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+      e.preventDefault();
+      deleteCookie('userId');
+      deleteCookie('userName');
+      deleteCookie('userRole');
+      deleteCookie('isUserActive');
+      deleteCookie('authToken');
+      router.refresh();
+    };
   return (
     <div
       className="w-96 h-[40rem]"
     >
       <button
         className="border-violet-500 rounded-sm border-2"
-        onClick={onLogout}
+        onClick={logout}
       >
         logout
       </button>
