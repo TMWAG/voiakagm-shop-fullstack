0a95f88493a828a2b23b6d77e6482111ce37561b
TMWAG <semyon.valyaev@gmail.com>
2023-06-18 20:17:58 +0300

    новые элементы

--- src/app/components/UserPanel.tsx
+++ src/app/components/UserPanel.tsx
@@ -2,20 +2,35 @@ import person from '@/common/icons/person.svg';
 import Image from "next/image";
 import Hoverable from '@/components/UI/Hoverable';
 import AuthPopup from './AuthPopup';
+import landmark from '@/common/icons/landmark.svg';
+import cart from '@/common/icons/cart.svg';
+import favorites from '@/common/icons/favorites.svg';
 import { cookies } from 'next/headers';
 import MiniProfile from './MiniProfile';
+import Link from 'next/link';
+import LogoutButton from './LogoutButton';
+
 
 export const UserPanel = () => {
   
   return (
-    <div className="flex items-center relative">
+    <div className="flex items-center justify-around relative w-48">
+      <Link
+        href='/contacts'
+        className='flex items-center text-violet-500'
+      >
+        <Image
+          src={landmark}
+          alt=''
+        />
+        Пермь 
+      </Link>
       <Hoverable
         item={
           <Image
-            className='border-2 rounded-full border-black'
             src={person}
             alt=''
-            width={32}
+            width={22}
           />
         }
         childrenClassName='end-0 top-0 rounded-2xl border-2 border-violet-500 bg-white'
@@ -25,6 +40,25 @@ export const UserPanel = () => {
           :<AuthPopup/>
         }
       </Hoverable>
+      {cookies().has('authToken') &&
+        <>
+        <Link href='/cart'>
+          <Image
+            src={cart}
+            alt=''
+          />
+        </Link>
+        <LogoutButton/>
+        <Link href='/favorites'>
+          <Image
+            alt=''
+            src={favorites}
+          />
+        </Link>
+        </>
+        
+      }
+      
     </div>
   );
 };
