c8458d57f51662beca07c5048e187dbaecde3f5c
TMWAG <semyon.valyaev@gmail.com>
2023-06-18 20:20:33 +0300

    кнопка выхода вынесена на панель навигации

--- src/app/components/MiniProfile.tsx
+++ src/app/components/MiniProfile.tsx
@@ -1,30 +1,12 @@
 'use client';
 
-import { deleteCookie } from "cookies-next";
-import { useRouter } from "next/navigation";
-
 export default function MiniProfile() {
-  const router = useRouter();
-  const logout = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-    ) => {
-      e.preventDefault();
-      deleteCookie('userId');
-      deleteCookie('userName');
-      deleteCookie('userRole');
-      deleteCookie('isUserActive');
-      deleteCookie('authToken');
-      router.refresh();
-    };
   return (
     <div
       className="w-96 h-[40rem]"
     >
-      <button
-        className="border-violet-500 rounded-sm border-2"
-        onClick={logout}
-      >
-        logout
-      </button>
+      
+      
     </div>
   );
 }
