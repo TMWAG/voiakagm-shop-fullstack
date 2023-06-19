8622082a54b628f56049e165d5fdf94e2bb78419
TMWAG <semyon.valyaev@gmail.com>
2023-06-19 22:18:27 +0300

    изменены стили

--- src/app/components/Header/Navbar.tsx
+++ src/app/components/Header/Navbar.tsx
@@ -4,7 +4,7 @@ import { navbarLinks } from "./navbarLinks";
 
 export const Navbar = () => {
   return (
-    <nav className="pt-6 pb-4 w-full h-9 flex justify-around items-center">
+    <nav className="mx-32 mt-6 mb-4 w-full h-9 flex justify-between items-center">
       <Links links={navbarLinks}/>
       <UserPanel />
     </nav>
