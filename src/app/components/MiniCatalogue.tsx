763cd9f73469b993f20b61da04e8f23105baa6e3
TMWAG <semyon.valyaev@gmail.com>
2023-06-20 20:39:18 +0300

    форматирование

--- src/app/components/MiniCatalogue.tsx
+++ src/app/components/MiniCatalogue.tsx
@@ -16,16 +16,41 @@ export default async function MiniCatalogue(){
     }
   ).then(res => res.json());
   return (
-    <div className="mb-32">
-      <div className="flex flex-col mb-5">
-        <span className="font-medium text-xl">Каталог в <Link href='/catalogue' className="text-violet-500">миниатюре</Link></span>
-        <span className="font-normal text-zinc-400">В каталоге на сайте вы можете подобрать различные ПК комплектующие и аксессуары.<br /> Железки могут быть как новыми, так и Б/У.</span>
+    <div
+      className="mb-32"
+    >
+      <div
+        className="flex flex-col mb-5"
+      >
+        <span
+          className="font-medium text-xl"
+        >
+          Каталог в <Link
+            href='/catalogue'
+            className="text-violet-500"
+          >
+            миниатюре
+          </Link>
+        </span>
+        <span
+          className="font-normal text-zinc-400"
+        >
+          В каталоге на сайте вы можете подобрать различные ПК комплектующие и аксессуары.<br />
+          Железки могут быть как новыми, так и Б/У.
+        </span>
       </div>
-      <div className="grid grid-cols-4 gap-[0.375rem]">
+      <div
+        className="grid grid-cols-4 gap-[0.375rem]"
+      >
         {products.map((p) => <ProductCard key={p.id} product={p}/>)}
         <Link
           href='/catalogue'
-          className={`${inter.className} font-light text-2xl bg-white border-[1px] flex items-center justify-center flex-col p-3 shadow-none hover:shadow-xl hover:scale-110 hover:text-3xl ease-in-out delay-0 duration-100`}
+          className={
+            `${inter.className} font-light text-2xl bg-white
+            border-[1px] flex items-center justify-center flex-col
+            p-3 shadow-none hover:shadow-xl hover:scale-110 hover:text-3xl
+            ease-in-out delay-0 duration-100`
+          }
         >
           Перейти в каталог
         </Link>
