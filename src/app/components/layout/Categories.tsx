f7f518c288fd031cecc5d06bae6fa97e937742a2
TMWAG <semyon.valyaev@gmail.com>
2023-06-20 13:17:18 +0300

    задан класс h-fit

--- src/app/components/layout/Categories.tsx
+++ src/app/components/layout/Categories.tsx
@@ -5,7 +5,7 @@ export default async function Categories(){
   const categories: ICategoryItem[] = await fetch(process.env.NEXT_PUBLIC_API_URL!.concat('category/all'))
     .then(res => res.json());
   return (
-    <div className="ml-32 border-[1px] rounded w-[19.25rem]">
+    <div className="ml-32 border-[1px] h-fit rounded w-[19.25rem]">
       {categories.map((c) => <CategoryItem key={c.id} id={c.id} name={c.name} picture={c.picture} />)}
     </div>
   );
