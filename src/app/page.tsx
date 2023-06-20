06e0adc1edf06857edb6ccb59c71c7fa3a132013
TMWAG <semyon.valyaev@gmail.com>
2023-06-20 19:18:25 +0300

    добавлен плейсхолдер акций и мини-каталог

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import promo_placeholder from '../../public/promo_placeholder.png';
+import Image from "next/image";
+import MiniCatalogue from './components/MiniCatalogue';
+
 export default function Home() {
   return (
-    <div className="">
-      main page
+    <div className="flex flex-col gap-4">
+      <Image
+        src={promo_placeholder}
+        alt=''
+      />
+      <MiniCatalogue/>
     </div>
   );
 }
