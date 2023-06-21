d7c4d01aa0cfe78f472a48316cb42fb6e4477a84
TMWAG <semyon.valyaev@gmail.com>
2023-06-21 13:23:39 +0300

    отрисовка товаров

--- src/app/(pages)/catalogue/page.tsx
+++ src/app/(pages)/catalogue/page.tsx
@@ -1,7 +1,12 @@
-export default function CataloguePage(){
+import getProducts from "@/app/lib/getProducts";
+import ProductCard from "@/components/UI/ProductCard";
+
+export default async function CataloguePage(){
+  const products = await getProducts({limit: 20, sort: "price-desc"});
+  // console.log(products);  
   return (
-    <div>
-      catalogue
+    <div className="m-5 grid grid-cols-4 gap-2 w-2/3">
+      {products.map((p) => <ProductCard key={p.id} product={p}/>)}
     </div>
   );
 }
