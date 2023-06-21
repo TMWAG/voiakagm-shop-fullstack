2d560ad23177f6f52d95eb0df7042e23b72be627
TMWAG <semyon.valyaev@gmail.com>
2023-06-22 00:51:58 +0300

    изменена ширина блока

--- src/app/components/layout/Greeting.tsx
+++ src/app/components/layout/Greeting.tsx
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 export default function Greeting(){
   
   return (
-    <p className="text-lg mt-12 mx-32 mb-5 w-full cursor-default">
+    <p className="text-lg mt-12 mx-32 mb-5 cursor-default">
       Привет, <span className="text-violet-500">{cookies().get('userName')?.value || 'Камрад'}!</span>
     </p>
   );
