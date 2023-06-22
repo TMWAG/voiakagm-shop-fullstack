e3862e881e369106e91323eba6a0c80c3f150129
TMWAG <semyon.valyaev@gmail.com>
2023-06-22 21:43:51 +0300

    получение данных товара с сервера

--- src/app/(pages)/product/[id]/page.tsx
+++ src/app/(pages)/product/[id]/page.tsx
@@ -1,9 +1,15 @@
-export default function ProductPage({
+import { IProductInfo } from "@/lib/types";
+
+export default async function ProductPage({
   params,
 }: {
   params: { id: number };
 }){
+  const product: IProductInfo = await fetch(
+    process.env.NEXT_PUBLIC_API_URL!.concat(`product/id/${params.id}`),
+    { method: 'GET' },
+  ).then(res => res.json());
   return (
-    <div>product {params.id}</div>
+    <div>product {product.name}</div>
   );
 }
