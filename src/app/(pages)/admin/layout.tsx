e72d4c0ef75c21412759f3acbe08615d01400f1b
TMWAG <semyon.valyaev@gmail.com>
2023-06-22 21:54:49 +0300

    импорт панели навигации, удалены ненужные данные

--- src/app/(pages)/admin/layout.tsx
+++ src/app/(pages)/admin/layout.tsx
@@ -1,29 +1,6 @@
 import { cookies } from "next/headers";
-import Link from "next/link";
 import { redirect } from "next/navigation";
-
-const links: { href: string, text: string }[] = [{
-  href: 'products',
-  text: 'Товары',
-}, {
-  href: 'categories',
-  text: 'Категории',
-}, {
-  href: 'vendors',
-  text: 'Производители',
-}, {
-  href: 'parameters',
-  text: 'Параметры',
-}, {
-  href: 'recommendations',
-  text: 'Рекомендации',
-}, {
-  href: 'delivery_services',
-  text: 'Службы доставки',
-}, {
-  href: 'orders',
-  text: 'Заказы',
-}];
+import AdminNavLinks from "./components/AdminNavLinks";
 
 export default function AdminLayout({
   children,
@@ -36,17 +13,7 @@ export default function AdminLayout({
   return (
     <div className="w-2/3">
       <span className="mt-11 mb-5 block text-2xl">Администрирование</span>
-      <nav className="grid grid-flow-col-dense">
-        {links.map((l) => 
-          <Link
-            href={`/admin/${l.href}`}
-            key={l.href}
-            className="border w-44 text-center text-lg rounded-sm hover:scale-105 duration-100"
-          >
-            {l.text}
-          </Link>
-        )}
-      </nav>
+      <AdminNavLinks/>
       {children}
     </div>
   );
