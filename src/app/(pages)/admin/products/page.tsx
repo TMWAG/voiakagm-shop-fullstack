dec2d0ee756b32e4756445cf50b568c798f9b411
TMWAG <semyon.valyaev@gmail.com>
2023-06-22 22:05:52 +0300

    добавлена отрисвока товаров

--- src/app/(pages)/admin/products/page.tsx
+++ src/app/(pages)/admin/products/page.tsx
@@ -1,5 +1,11 @@
-export default function ProductsPage(){
+import getProducts from "@/app/lib/getProducts";
+import ProductCard from "@/components/UI/ProductCard";
+
+export default async function ProductsPage(){
+  const products = await getProducts({limit: 24});
   return (
-    <div>products</div>
+    <div className="grid grid-cols-6 gap-1 mb-12">
+      {products.map((p) => <ProductCard key={p.id} subPath="admin/products" product={p}/>)}
+    </div>
   );
 }
