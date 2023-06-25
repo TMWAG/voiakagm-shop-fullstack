37da80fb925e396b84e18ccd110d77aa432879b9
TMWAG <semyon.valyaev@gmail.com>
2023-06-25 23:59:15 +0300

    возвращено отображение имени авторизованного пользователя

--- src/app/components/layout/Greeting.tsx
+++ src/app/components/layout/Greeting.tsx
@@ -4,7 +4,7 @@ export default function Greeting(){
   
   return (
     <p className="text-lg mt-12 mx-32 mb-5 cursor-default">
-      Привет, <span className="text-violet-500">{cookies().get('userName')?.value || 'Камрад'}!</span>
+      Привет, <span className="text-violet-500">{cookies().get('user-name')?.value || 'Камрад'}!</span>
     </p>
   );
 }
