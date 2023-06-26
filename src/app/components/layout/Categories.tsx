26e511fb9e782edde48ea42086a195905eef4bcf
TMWAG <semyon.valyaev@gmail.com>
2023-06-27 00:37:26 +0300

    изменён способ получения категорий

--- src/app/components/layout/Categories.tsx
+++ src/app/components/layout/Categories.tsx
@@ -1,9 +1,8 @@
-import { ICategoryItem } from "@/lib/types";
 import CategoryItem from "./CategoryItem";
+import { prisma } from "@/lib/prisma";
 
 export default async function Categories(){
-  const categories: ICategoryItem[] = await fetch(process.env.NEXT_PUBLIC_API_URL!.concat('category/all'))
-    .then(res => res.json());
+  const categories = await prisma.category.findMany();
   return (
     <div className="ml-32 border-[1px] h-fit rounded w-[19.25rem]">
       {categories.map((c) => <CategoryItem key={c.id} id={c.id} name={c.name} picture={c.picture} />)}
