18738fb89fc0ebbba659bf0919015dc61d4d6e73
TMWAG <semyon.valyaev@gmail.com>
2023-06-26 23:42:10 +0300

    модальное окно с формой добавления категории

--- src/app/(pages)/admin/categories/page.tsx
+++ src/app/(pages)/admin/categories/page.tsx
@@ -1,5 +1,15 @@
+'use client';
+
+import Modal from "../components/Modal";
+import AddCategoryForm from "./components/AddCategoryForm";
+
 export default function CategoriesPage(){
   return (
-    <div>catagories</div>
+    <div className="grid grid-cols-6 gap-1 mb-12">
+      <Modal buttonText="Добавить категорию">
+        <AddCategoryForm/>
+      </Modal>
+      <div>catagories</div>
+    </div>
   );
 }
