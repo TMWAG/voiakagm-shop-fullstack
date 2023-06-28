616d3746388765aacc0a75039b5fddd2767d38fd
TMWAG <semyon.valyaev@gmail.com>
2023-06-28 16:59:29 +0300

    сортировка категорий, убрана модальная обёртка

--- src/app/(pages)/admin/categories/page.tsx
+++ src/app/(pages)/admin/categories/page.tsx
@@ -1,15 +1,15 @@
-'use client';
-
-import Modal from "../components/Modal";
+import { prisma } from "@/lib/prisma";
 import AddCategoryForm from "./components/AddCategoryForm";
+import CategoryCard from "./components/CategoryCard";
 
-export default function CategoriesPage(){
+export default async function CategoriesPage(){
+  const categories = await prisma.category.findMany({
+    orderBy: { id: "asc" },
+  });
   return (
     <div className="grid grid-cols-6 gap-1 mb-12">
-      <Modal buttonText="Добавить категорию">
-        <AddCategoryForm/>
-      </Modal>
-      <div>catagories</div>
+      <AddCategoryForm/>
+      {categories.map((c) => <CategoryCard category={c} key={c.id}/>)}
     </div>
   );
 }
