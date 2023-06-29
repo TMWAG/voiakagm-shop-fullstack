e019918e0e0e521e426fc9f5d389987091d2cb09
TMWAG <semyon.valyaev@gmail.com>
2023-06-29 23:14:51 +0300

    страница администрирования производителей

--- src/app/(pages)/admin/vendors/page.tsx
+++ src/app/(pages)/admin/vendors/page.tsx
@@ -1,5 +1,15 @@
-export default function VendorsPage(){
+import { prisma } from "@/lib/prisma";
+import AddVendorForm from "./components/AddVendorForm";
+import VendorCard from "./components/VendorCard";
+
+export default async function VendorsPage(){
+  const vendors = await prisma.vendor.findMany({
+    orderBy: { id: 'asc' },
+  });
   return (
-    <div>vendors</div>
+    <div className="grid grid-cols-6 gap-1 mb-12">
+      <AddVendorForm/>
+      {vendors.map((v) => <VendorCard vendor={v} key={v.id} />)}
+    </div>
   );
 }
