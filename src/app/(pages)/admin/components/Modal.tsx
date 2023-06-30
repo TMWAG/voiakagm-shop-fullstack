04f1db9a3abeef2c2ed00f6ee3a90e3cdda9c249
TMWAG <semyon.valyaev@gmail.com>
2023-06-30 23:58:35 +0300

    изменение модального окна для нового функционала

--- src/app/(pages)/admin/components/Modal.tsx
+++ src/app/(pages)/admin/components/Modal.tsx
@@ -1,42 +1,27 @@
-'use client';
-
-import { useRef, useState } from "react";
-
 export default function Modal({
-  buttonText,
-  children,
+  button,
+  content,
+  hidden,
+  overlayRef,
+  onClick,
 }: {
-  buttonText: string;
-  children: React.ReactNode;
+  button: React.ReactElement<HTMLButtonElement>;
+  content: React.ReactNode;
+  hidden: boolean;
+  overlayRef: React.MutableRefObject<HTMLDivElement | null>;
+  onClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }) {
-  const [hidden, setHidden] = useState<boolean>(true);
-  const overlay = useRef<HTMLDivElement | null>(null);
   return (
     <>
-      <button
-        className="
-          border-[1px] h-24 bg-white flex
-          flex-col justify-between p-3 hover:shadow-xl
-          hover:scale-110 shadow-none ease-in-out
-          delay-0 duration-100
-        "
-        onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-          e.preventDefault();
-          setHidden(!hidden);
-        }}
-      >
-        {buttonText}
-      </button>
+      {button}
       {!hidden &&
         <div
-          ref={overlay}
+          ref={overlayRef}
           className="
             text-black fixed top-0 left-0
             z-10 w-screen h-screen bg-black/25
           "
-          onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-            if (e.target === overlay.current) setHidden(!hidden);
-          }}
+          onClick={onClick}
         >
           <div
             className="
@@ -47,7 +32,7 @@ export default function Modal({
               bg-white
             "
           >
-            {children}
+            {content}
           </div>
         </div>
       }
