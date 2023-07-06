bb704a0a4e9af17b3be559e4736fd0ad3cd7731c
TMWAG <semyon.valyaev@gmail.com>
2023-07-06 14:19:14 +0300

    страница для параметров категории

---
+++ src/app/(pages)/admin/parameters/[id]/page.tsx
@@ -0,0 +1,31 @@
+import { prisma } from "@/lib/prisma";
+import AddParameterForm from "../components/AddParameterForm";
+import ParameterCard from "../components/ParameterCard";
+
+export default async function ParameterAdminPage({
+  params,
+}: {
+  params: {
+    id: number,
+  }
+}) {
+  const parameters = await prisma
+    .productParameter
+    .findMany({ 
+      where: { 
+        categoryId: Number(params.id),
+      },
+    });
+  return (
+    <div
+      className="
+        grid grid-flow-col-dense grid-cols-7 gap-2 my-2
+      "
+    >
+      <AddParameterForm categoryId={params.id} />
+      {parameters.length > 0 &&
+        parameters.map((p) => <ParameterCard key={p.id} parameter={p} />)
+      }
+    </div>
+  );
+}
