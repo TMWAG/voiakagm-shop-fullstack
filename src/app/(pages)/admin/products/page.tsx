cffb208c29a46f31e632e78178896a4639cdec33
TMWAG <semyon.valyaev@gmail.com>
2023-07-06 17:51:19 +0300

    страница по-умолчанию

--- src/app/(pages)/admin/products/page.tsx
+++ src/app/(pages)/admin/products/page.tsx
@@ -1,11 +1,11 @@
-import getProducts from "@/app/lib/getProducts";
-import ProductCard from "@/components/UI/ProductCard";
-
 export default async function ProductsPage(){
-  const products = await getProducts({limit: 24});
   return (
-    <div className="grid grid-cols-6 gap-1 mb-12">
-      {products.map((p) => <ProductCard key={p.id} subPath="admin/products" product={p}/>)}
+    <div className="
+      flex flex-col h-96 items-center
+    ">
+      <span
+        className="block my-48 text-3xl"
+      >Выберите категорию товаров сверху</span>
     </div>
   );
 }
