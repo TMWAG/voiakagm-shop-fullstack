1e0b45772b21ebd8516f1efb5ab2c94e93054a37
TMWAG <semyon.valyaev@gmail.com>
2023-07-06 14:00:34 +0300

    добавлено навигационная панель

---
+++ src/app/(pages)/admin/parameters/layout.tsx
@@ -0,0 +1,24 @@
+import { prisma } from "@/lib/prisma";
+import NavLink from "./components/NavLink";
+
+export default async function AdminParametersLayout({
+  children,
+}: {
+  children: React.ReactNode
+}){
+  const categories = await prisma.category.findMany({
+    orderBy: {id: "asc"},
+  });
+  return (
+    <div>
+      <nav
+        className="
+          grid grid-flow-col-dense grid-cols-7 gap-2
+        "
+      >
+        {categories.map((c) => <NavLink id={c.id} name={c.name} key={c.id}/>)}
+      </nav>
+      {children}
+    </div>
+  );
+}
