5cd2ee37143e9695e07a6e7405b9264b2ce0ec5a
TMWAG <semyon.valyaev@gmail.com>
2023-07-10 20:01:32 +0300

    удалён интерфейс IProductItem

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -16,21 +16,6 @@ export interface IVendorItem {
   picture: string | null;
 }
 
-export interface IProductItem {
-  id: number;
-  name: string;
-  categoryId: number;
-  price: number;
-  discount: number | null;
-  description: string;
-  amount: number | null;
-  sold: number;
-  used: boolean;
-  category: { name: string };
-  vendor: { name : string };
-  pictures: { filename: string }[];
-}
-
 export interface IProductInfo {
   id: number;
   name: string;
