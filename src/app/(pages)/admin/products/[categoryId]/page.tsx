943d42f51823d8765b230be55b428314f0d47715
TMWAG <semyon.valyaev@gmail.com>
2023-07-10 23:22:35 +0300

    переименован динамический путь

--- src/app/(pages)/admin/products/[categoryId]/page.tsx
+++ src/app/(pages)/admin/products/[categoryId]/page.tsx
@@ -5,10 +5,10 @@ import ProductCard from "@/components/UI/ProductCard";
 export default async function ProductAdminPage({
   params,
 }: {
-  params: { id: number };
+  params: { categoryId: number };
 }){
   const products = await prisma.product.findMany({
-    where: { categoryId: Number(params.id) },
+    where: { categoryId: Number(params.categoryId) },
     include: { pictures: { select: { filename: true } } },
   });
   const vendors = await prisma.vendor.findMany();
@@ -16,9 +16,9 @@ export default async function ProductAdminPage({
     <div className="grid grid-cols-6 gap-2 mb-12">
       <AddProductButtonWithModal
         vendors={vendors}
-        categoryId={params.id}
+        categoryId={params.categoryId}
       />
-      {products.map((p) => <ProductCard product={p} subPath='admin' key={p.id}/>)}
+      {products.map((p) => <ProductCard product={p} subPath={`admin/products/${params.categoryId}/`} key={p.id}/>)}
     </div>
   );
 }
