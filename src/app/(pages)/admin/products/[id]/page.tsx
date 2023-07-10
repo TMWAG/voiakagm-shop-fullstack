9e0b815d6cb17b802c235947a2f8e8d85f34d0f0
TMWAG <semyon.valyaev@gmail.com>
2023-07-10 21:53:26 +0300

    отрисовка товаров, кнопка добавления товара

--- src/app/(pages)/admin/products/[id]/page.tsx
+++ src/app/(pages)/admin/products/[id]/page.tsx
@@ -1,11 +1,24 @@
-import { IProductInfo } from "@/lib/types";
+import { prisma } from "@/lib/prisma";
+import AddProductButtonWithModal from "../components/AddProductButtonWithModal";
+import ProductCard from "@/components/UI/ProductCard";
 
 export default async function ProductAdminPage({
   params,
 }: {
   params: { id: number };
 }){
+  const products = await prisma.product.findMany({
+    where: { categoryId: Number(params.id) },
+    include: { pictures: { select: { filename: true } } },
+  });
+  const vendors = await prisma.vendor.findMany();
   return (
-    <div>{params.id}</div>
+    <div className="grid grid-cols-6 gap-2 mb-12">
+      <AddProductButtonWithModal
+        vendors={vendors}
+        categoryId={params.id}
+      />
+      {products.map((p) => <ProductCard product={p} subPath='admin' key={p.id}/>)}
+    </div>
   );
 }
