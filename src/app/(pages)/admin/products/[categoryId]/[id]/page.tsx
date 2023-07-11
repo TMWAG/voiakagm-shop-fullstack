9d54b54fb635a76a355ef57ff80d8d53e61c48bd
TMWAG <semyon.valyaev@gmail.com>
2023-07-11 21:14:18 +0300

    получение товара с необходимыми полями, отрисовка характеристик

--- src/app/(pages)/admin/products/[categoryId]/[id]/page.tsx
+++ src/app/(pages)/admin/products/[categoryId]/[id]/page.tsx
@@ -1,11 +1,39 @@
-export default function AdminProductPage({
+import { prisma } from "@/lib/prisma";
+import ProductInfo from "../../components/ProductInfo";
+
+export default async function AdminProductPage({
   params,
 }: {
   params: { id: number };
 }){
+  const product = await prisma.product.findUniqueOrThrow({
+    where: { id: Number(params.id) },
+    include: { 
+      pictures: true,
+      category: {
+        select: { name: true },
+      },
+      vendor: {
+        select: { name: true },
+      },
+      characteristics: {
+        orderBy: { id: "asc" },
+        include: { 
+          parameter: { 
+            select: { name: true },
+          },
+        },
+      },
+    },
+  });
   return (
-    <div>
-      {params.id}
+    <div
+      className="
+        border mt-2 bg-white rounded-sm
+        grid grid-flow-col grid-rows-3
+      "
+    >
+      <ProductInfo product={product}/>
     </div>
   );
 }
