85aea4fe7329dabfa517dfad951486cabbe86e6c
TMWAG <semyon.valyaev@gmail.com>
2023-07-11 21:35:35 +0300

    новый компонент для отображения информации о товаре

---
+++ src/app/(pages)/admin/products/components/ProductInfo.tsx
@@ -0,0 +1,75 @@
+import { type Prisma } from "@prisma/client";
+import ProductInfoItem from "./PriductInfoItem";
+
+export default function ProductInfo({
+  product,
+}: {
+  product: Prisma.ProductGetPayload<{
+    include: {
+      pictures: true,
+      category: {
+        select: { name: true },
+      },
+      vendor: {
+        select: { name: true },
+      },
+      characteristics: {
+        orderBy: { id: "asc" },
+        include: {
+          parameter: {
+            select: { name: true },
+          },
+        },
+      },
+    },
+  }>
+}) {
+  return (
+    <div
+      className="
+        flex flex-col items-center bg-red-200 w-fit
+      "
+    >
+      <span className="block text-center text-xl">{product.name}</span>
+      <ProductInfoItem
+        infoName="Категория"
+        infoValue={product.category.name}
+      />
+      <ProductInfoItem
+        infoName="Производитель"
+        infoValue={product.vendor.name}
+      />
+      <ProductInfoItem
+        infoName="Цена"
+        infoValue={
+          (product.price/100)
+            .toLocaleString('ru', {style: 'currency', currency: 'RUB'})
+        }
+      />
+      <ProductInfoItem
+        infoName="Скидка"
+        infoValue={
+          product.discount
+          ?`${product.discount}%`
+          : 'нет'
+        }
+      />
+      <ProductInfoItem
+        infoName="Б/У"
+        infoValue={
+          product.used
+          ?'Да'
+          :'Нет'
+        }
+      />
+      <ProductInfoItem
+        infoName="Количество"
+        infoValue={product.amount}
+      />
+      <ProductInfoItem
+        infoName="Продано"
+        infoValue={product.sold}
+      />
+    </div>
+  );
+}
