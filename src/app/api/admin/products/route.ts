7b87daa859d7e9776e0658e839ad1c0c336b1312
TMWAG <semyon.valyaev@gmail.com>
2023-07-13 23:56:21 +0300

    функция для обработки put запросов

--- src/app/api/admin/products/route.ts
+++ src/app/api/admin/products/route.ts
@@ -1,5 +1,5 @@
 import { getErrorResponse } from "@/lib/helpers";
-import { CreateProductSchema, type CreateProductInput } from "@/lib/validations/product.schema";
+import { CreateProductSchema, type CreateProductInput, UpdateProductInput, UpdateProductSchema } from "@/lib/validations/product.schema";
 import { NextRequest, NextResponse } from "next/server";
 import { ZodError } from "zod";
 import { getUserRole } from "../../_lib/utils";
@@ -8,10 +8,10 @@ import { prisma } from "@/lib/prisma";
 
 export async function POST(req: NextRequest){
   try {
-    if (await getUserRole(req) !== Role.ADMIN)
+    if (await getUserRole(req) !== Role.ADMIN){
       return getErrorResponse(403, 'Недостаточный уровень привилегий');
+    }
     const body = await req.json() as CreateProductInput;
-    console.log('-------------\n', body);
     const data = CreateProductSchema.parse(body);
     const product = await prisma.product.create({
       data,
@@ -27,5 +27,31 @@ export async function POST(req: NextRequest){
     if (error instanceof ZodError)
       return getErrorResponse(400, 'Данные имеют неверный формат');
     return getErrorResponse(500, error.message);    
+  }
+}
+
+export async function PUT(req: NextRequest){
+  try {
+    if (await getUserRole(req) !== Role.ADMIN) {
+      return getErrorResponse(403, 'Недостаточный уровень привилегий');
+    }
+    const body = await req.json() as UpdateProductInput;
+    const data = UpdateProductSchema.parse(body);
+    console.log('---------\n', data);
+    const product = await prisma.product.update({
+      where: { id: body.id },
+      data,
+    });
+    return new NextResponse(JSON.stringify({
+      status: 'success',
+      data: product,
+    }), {
+      status: 201,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error: any) {
+    if (error instanceof ZodError)
+      return getErrorResponse(400, 'Данные имеют неверный формат');
+    return getErrorResponse(500, error.message); 
   }
 }
