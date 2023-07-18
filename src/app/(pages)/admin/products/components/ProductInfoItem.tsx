517997f8ea73db185255eb74259a2bbe8496577f
TMWAG <semyon.valyaev@gmail.com>
2023-07-18 18:06:36 +0300

    увеличен размер шрифта

--- src/app/(pages)/admin/products/components/ProductInfoItem.tsx
+++ src/app/(pages)/admin/products/components/ProductInfoItem.tsx
@@ -6,7 +6,7 @@ export default function ProductInfoItem({
   infoValue: string | number;
 }){
   return (
-    <div className="p-1 w-full flex justify-between even:bg-zinc-100">
+    <div className="p-1 w-full text-lg flex justify-between even:bg-zinc-100">
       <span className="block">{infoName}:</span>
       <span className="text-violet-500 block">{infoValue}</span>
     </div>
