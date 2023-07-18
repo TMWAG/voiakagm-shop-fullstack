b9f8b3badcdd450668e047362e35de1744c798a0
TMWAG <semyon.valyaev@gmail.com>
2023-07-18 23:01:04 +0300

    изменено приветственное сообщение и стилизация

--- src/app/(pages)/admin/parameters/page.tsx
+++ src/app/(pages)/admin/parameters/page.tsx
@@ -1,5 +1,9 @@
 export default function ParametersPage(){
   return (
-    <div>parameters</div>
+    <div className="flex flex-col h-96 items-center">
+      <span
+        className="block my-48 text-3xl"
+      >Выберите категорию, для которой хотите добавить параметр</span>
+    </div>
   );
 }
