cc0516caff528d5d87ea735a420006363b474c63
TMWAG <semyon.valyaev@gmail.com>
2023-07-18 18:04:28 +0300

    эндпоинт для удаления товара

--- src/app/api/admin/products/route.ts
+++ src/app/api/admin/products/route.ts
@@ -1,10 +1,11 @@
 import { getErrorResponse } from "@/lib/helpers";
-import { CreateProductSchema, type CreateProductInput, UpdateProductInput, UpdateProductSchema } from "@/lib/validations/product.schema";
+import { CreateProductSchema, type CreateProductInput, UpdateProductInput, UpdateProductSchema, DeleteProductSchema, DeleteProductInput } from "@/lib/validations/product.schema";
 import { NextRequest, NextResponse } from "next/server";
 import { ZodError } from "zod";
 import { getUserRole } from "../../_lib/utils";
 import { Role } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 export async function POST(req: NextRequest){
   try {
@@ -47,6 +48,32 @@ export async function PUT(req: NextRequest){
     }), {
       status: 201,
       headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error: any) {
+    if (error instanceof ZodError)
+      return getErrorResponse(400, 'Данные имеют неверный формат');
+    return getErrorResponse(500, error.message); 
+  }
+}
+
+export async function DELETE(req: NextRequest){
+  try {
+    if (await getUserRole(req) !== Role.ADMIN) {
+      return getErrorResponse(403, 'Недостаточный уровень привилегий');
+    }
+    const { searchParams } = new URL(req.url);
+    const data = DeleteProductSchema.parse({
+      id: Number(searchParams.get('id')),
+    }) as DeleteProductInput;
+    await prisma.product.delete({
+      where: { id: data.id },
+    });
+    revalidatePath('/(pages)/admin/products/[categoryId]');
+    return new NextResponse(JSON.stringify({
+      status: 'success',
+    }), {
+      status: 200,
+      headers: {'Content-Type': 'application/json'},
     });
   } catch (error: any) {
     if (error instanceof ZodError)
