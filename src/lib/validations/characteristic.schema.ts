e9b1325f0cd880e8e5e4f702d680f4cc1ed85228
TMWAG <semyon.valyaev@gmail.com>
2023-07-19 23:05:50 +0300

    схемы и типы для взамодействия с характеристиками товаров

---
+++ src/lib/validations/characteristic.schema.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+
+export const CreateCharacteristicSchema = z.object({
+  parameterId: z
+    .number()
+    .int()
+    .positive(),
+  productId: z
+    .number()
+    .int()
+    .positive(),
+  value: z
+    .string()
+    .min(1),
+});
+
+export type CreateCharacteristicInput = z.infer<typeof CreateCharacteristicSchema>;
+
+export const UpdateCharacteristicSchema = z.object({
+  id: z
+    .number()
+    .int()
+    .positive(),
+  parameterId: z
+    .number()
+    .int()
+    .positive(),
+  value: z
+    .string()
+    .min(1),
+});
+
+export type UpdateCharacteristicInput = z.infer<typeof UpdateCharacteristicSchema>;
+
+export const DeleteCharacteristicSchema = z.object({
+  id: z
+    .number()
+    .positive()
+    .int(),
+});
+
+export type DeleteCharacteristicInput = z.infer<typeof DeleteCharacteristicSchema>;
