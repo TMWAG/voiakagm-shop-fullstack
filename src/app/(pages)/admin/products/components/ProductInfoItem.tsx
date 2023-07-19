cca8482fdde8889201759f7303a201d04d9d0df7
TMWAG <semyon.valyaev@gmail.com>
2023-07-20 02:03:34 +0300

    уменьшен размер текста, текст значений затемнён

--- src/app/(pages)/admin/products/components/ProductInfoItem.tsx
+++ src/app/(pages)/admin/products/components/ProductInfoItem.tsx
@@ -6,9 +6,9 @@ export default function ProductInfoItem({
   infoValue: string | number;
 }){
   return (
-    <div className="p-1 w-full text-lg flex justify-between even:bg-zinc-100">
+    <div className="p-1 w-full flex justify-between even:bg-zinc-100">
       <span className="block">{infoName}:</span>
-      <span className="text-violet-500 block">{infoValue}</span>
+      <span className="text-violet-600 block text-end">{infoValue}</span>
     </div>
   );
 }
