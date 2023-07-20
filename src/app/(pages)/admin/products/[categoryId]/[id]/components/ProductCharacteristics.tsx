77309cb580ffb887cc32e59e6e77f65170accb21
TMWAG <semyon.valyaev@gmail.com>
2023-07-20 21:17:15 +0300

    реализован функционал изменения характеристик

--- src/app/(pages)/admin/products/[categoryId]/[id]/components/ProductCharacteristics.tsx
+++ src/app/(pages)/admin/products/[categoryId]/[id]/components/ProductCharacteristics.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import AddCharacteristicButtonWithModal from "./AddCharacteristicButtonWithModal";
+import EditCharacteristicButtonWithModal from "./EditCharacteristicButtonWithModal";
 
 export default async function ProductCharacteristics({
   productId,
@@ -9,6 +10,7 @@ export default async function ProductCharacteristics({
   productId: number,
 }) {
   const characteristics = await prisma.productCharacteristic.findMany({
+    orderBy: { parameter: { name: "asc" } },
     where: { productId },
     include: { parameter: { select: { name: true } } },
   });
@@ -16,27 +18,23 @@ export default async function ProductCharacteristics({
     where: { categoryId },
   });
   return (
-    <div className="flex flex-col bg-red-200 items-center">
-      <table className="text-start w-full">
-        <thead>
-          <tr>
-            <th>Параметр</th>
-            <th>Значение</th>
-            <th>Удалить</th>
-          </tr>
-        </thead>
-        <tbody>
-          {characteristics.length > 0 
-            ?characteristics.map((c) => (
-              <tr key={c.id}>
-                <td>{c.parameter.name}</td>
-                <td>{c.value}</td>
-              </tr>
-            ))
-            : <tr><td colSpan={3}>Характеристики не заданы</td></tr>
-          }
-        </tbody>
-      </table>
+    <div className="flex flex-col items-center">
+      <span className="font-bold text-lg">Характеристики</span>
+      {characteristics.length > 0 
+        ?characteristics.map((c) => (
+          <div className="w-full flex items-center" key={c.id}>
+            <span className="block w-7/12 border-b-violet-200 border-b border-dashed">{c.parameter.name}</span>
+            <span className="block w-3/12">{c.value}</span>
+            <EditCharacteristicButtonWithModal
+              id={c.id}
+              initialValue={c.value}
+              parameterName={c.parameter.name}
+            />
+            <button>del</button>
+          </div>
+        ))
+        : <span>Характеристики не заданы</span>
+      }
       <AddCharacteristicButtonWithModal parameters={parameters} productId={productId}/>
     </div>
   );
